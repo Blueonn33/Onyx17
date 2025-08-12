3b50a8e6b73667a17a6440ab889de87fc428a30b
Martin Marinov <m.marinov1733@gmail.com>
2025-08-12 23:39:17 +0300

    създаване и извеждане на езици

---
+++ Controllers/DocumentationController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onyx17.Models;
+using Onyx17.Repositories.Interfaces;
+using Onyx17.ViewModels;
+
+namespace Onyx17.Controllers
+{
+    public class DocumentationController : Controller
+    {
+        private readonly ILanguageRepository _repository;
+
+        public DocumentationController(ILanguageRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var languages = await _repository.GetAllLanguagesAsync();
+            return View(languages);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(LanguageViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                byte[]? imageData = null;
+                string? mimeType = null;
+
+                if (model.ImageFile != null && model.ImageFile.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        await model.ImageFile.CopyToAsync(ms);
+                        imageData = ms.ToArray();
+                        mimeType = model.ImageFile.ContentType;
+                    }
+                }
+
+                var language = new Language
+                {
+                    Name = model.Name,
+                    ImageData = imageData,
+                    ImageMimeType = mimeType
+                };
+
+                await _repository.CreateLanguageAsync(language);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLanguageImage(int languageId)
+        {
+            var language = await _repository.GetLanguageByIdAsync(languageId);
+
+            if(language == null || language.ImageData == null || language.ImageMimeType == null)
+            {
+                return NotFound();
+            }
+
+            return File(language.ImageData, language.ImageMimeType);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int languageId)
+        {
+            var language = await _repository.GetLanguageByIdAsync(languageId);
+
+            if(language == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.DeleteLanguageAsync(languageId);
+
+            return Ok();
+        }
+    }
+}
