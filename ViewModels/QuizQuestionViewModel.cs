05258bb6e38f39749bc0f8809a95c72774f6c41e
Martin Marinov <m.marinov1733@gmail.com>
2025-08-14 23:11:34 +0300

    въпроси за тестовете

---
+++ ViewModels/QuizQuestionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Onyx17.ViewModels
+{
+    public class QuizQuestionViewModel
+    {
+        public int Id { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int QuizId { get; set; }
+    }
+}
