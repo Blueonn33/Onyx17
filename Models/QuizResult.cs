46074c94a95795d3b205dc643f7cc8d66ac91d8d
Martin Marinov <m.marinov1733@gmail.com>
2025-08-15 11:53:59 +0300

    корекции

---
+++ Models/QuizResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Onyx17.Models
+{
+    public class QuizResult
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(QuizId))]
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+        
+        public int CorrectAnswers { get; set; }
+        public int TotalQuestions { get; set; }
+        public DateTime CompletionDate { get; set; } = DateTime.Now;
+    }
+}
