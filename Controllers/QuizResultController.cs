80f569a4bef5022d1a613ca47cf23c95c7b2646a
Martin Marinov <m.marinov1733@gmail.com>
2025-08-28 15:56:32 +0300

    извежда последния резултат от тестовете

--- Controllers/QuizResultController.cs
+++ Controllers/QuizResultController.cs
@@ -18,64 +18,66 @@ namespace Onyx17.Controllers
             _userManager = userManager;
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> Index(int quizId)
-        //{
-        //    var user = await _userManager.GetUserAsync(User);
-
-        //    if(user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    string userId = user.Id;
-
-        //    if (quizId == 0)
-        //    {
-        //        return RedirectToAction("Index", "Quiz");
-        //    }
-
-        //    var quizResults = await _repository.GetQuizResultsByQuizAndUserIdAsync(quizId, userId);
-        //    return View(quizResults);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Create(int quizId, QuizResultViewModel model)
-        //{
-        //    var user = await _userManager.GetUserAsync(User);
-
-        //    if(user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    string userId = user.Id;
-
-        //    if (model == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var quizResult = new QuizResult
-        //    {
-        //        Score = model.Score,
-        //        TotalQuestions = model.TotalQuestions,
-        //        QuizId = quizId,
-        //        UserId = userId
-        //    };
-
-        //    await _repository.CreateQuizResultAsync(quizResult);
-        //    ViewBag.QuizId = quizId;
-        //    return View(quizResult);
-        //}
-
         [HttpGet]
-        public async Task<IActionResult> Index(int quizId, int correct, int total)
+        public async Task<IActionResult> Index(int quizId)
         {
-            ViewBag.QuizId = quizId;
-            ViewBag.Correct = correct;
-            ViewBag.Total = total;
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            string userId = user.Id;
+
+            if (quizId == 0)
+            {
+                return RedirectToAction("Index", "Quiz");
+            }
+
+            var quizResults = await _repository.GetQuizResultsByQuizAndUserIdAsync(quizId, userId);
+            var lastResult = quizResults.OrderByDescending(q => q.TakenAt).FirstOrDefault();
+
+            return View(lastResult);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(int quizId, QuizResultViewModel model)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            string userId = user.Id;
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            var quizResult = new QuizResult
+            {
+                Score = model.Score,
+                TotalQuestions = model.TotalQuestions,
+                QuizId = quizId,
+                UserId = userId
+            };
+
+            await _repository.CreateQuizResultAsync(quizResult);
+            ViewBag.QuizId = quizId;
+            return View(quizResult);
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index(int quizId, int correct, int total)
+        //{
+        //    ViewBag.QuizId = quizId;
+        //    ViewBag.Correct = correct;
+        //    ViewBag.Total = total;
+        //    return View();
+        //}
     }
 }
