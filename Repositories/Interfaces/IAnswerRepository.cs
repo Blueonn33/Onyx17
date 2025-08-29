de0458e8b6050974445b4aab86c38a78cac4d77a
Martin Marinov <m.marinov1733@gmail.com>
2025-08-29 16:33:23 +0300

    проблем с профила на отговорите и часа

--- Repositories/Interfaces/IAnswerRepository.cs
+++ Repositories/Interfaces/IAnswerRepository.cs
@@ -4,7 +4,7 @@ namespace Onyx17.Repositories.Interfaces
 {
     public interface IAnswerRepository
     {
-        public Task<IEnumerable<Answer>> GetAllAnswersByQuestionIdAsync(int questionId);
+        public Task<IEnumerable<Answer>> GetAllAnswersByQuestionAndUserIdAsync(int questionId, string userId);
         public Task<Answer?> GetAnswerByIdAsync(int answerId);
         public Task CreateAnswerAsync(Answer answer);
         public Task UpdateAnswerAsync(Answer answer);
